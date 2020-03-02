5868bc580f7d10cce973ae7af112d2448eb597f3
Roland Varga <roland.varga.can@gmail.com>
2020-03-02 11:48:03 -0500

    update config

--- config.go
+++ config.go
@@ -3,24 +3,32 @@ package main
 import (
 	"io/ioutil"
 	"log"
+	"os"
 
 	"gopkg.in/yaml.v2"
 )
 
 // Config the configuration of the application
 type Config struct {
-	AlertmanagerAPI string `yaml:"alertmanager_api"`
+	AlertmanagerURL string `yaml:"alertmanager_url"`
 }
 
-func loadConfig(path string) (conf *Config, err error) {
-	f, err := ioutil.ReadFile(path)
-	if err != nil {
-		return nil, err
+func loadConfig(path string) (*Config, error) {
+	conf := &Config{}
+	if path != "" {
+		f, err := ioutil.ReadFile(path)
+		if err != nil {
+			return nil, err
+		}
+		err = yaml.Unmarshal([]byte(f), &conf)
+		if err != nil {
+			return nil, err
+		}
 	}
 
-	err = yaml.Unmarshal([]byte(f), &conf)
-	if err != nil {
-		return nil, err
+	envURL := os.Getenv("ALERTMANAGER_URL")
+	if envURL != "" {
+		conf.AlertmanagerURL = envURL
 	}
 
 	log.Printf("Config loaded, path: %s", path)
